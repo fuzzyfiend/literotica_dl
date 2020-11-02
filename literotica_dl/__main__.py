08b212b6734b4d0895742d0e68d7fab4e278bdb4
FuzzyFiend <50032576+fuzzyfiend@users.noreply.github.com>
2020-11-01 23:54:20 -0600

    add argparse skeleton

--- literotica_dl/__main__.py
+++ literotica_dl/__main__.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging
+import argparse
 
 handler = logging.StreamHandler()
 handler.setFormatter(
@@ -13,6 +14,10 @@ log = logging.getLogger("literotica_dl")
 log.setLevel(logging.INFO)
 log.addHandler(handler)
 
-
 def main():
+    # Handle arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-V', '--version', action='version', version='v0.0.1')
+    parser.add_argument('-v', '--verbose', action='count', default=0)
+    args = parser.parse_args()
     return
